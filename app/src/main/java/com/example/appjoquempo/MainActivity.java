00f4217fbfd9baa3d9fa133d8473dfcab164ca74
Lucca465 <lucca465@gmail.com>
2021-10-13 21:51:17 -0300

    Criando a lógica do jogo

--- app/src/main/java/com/example/appjoquempo/MainActivity.java
+++ app/src/main/java/com/example/appjoquempo/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.appjoquempo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*
+    Pedra = 0
+    Papel = 1
+    Tesoura = 2
+     */
+
+    public void selecionadoPedra(View view){
+        this.opcaoSelecionado(0);
+    }
+
+    public void selecionadoPapel(View view){
+        this.opcaoSelecionado(1);
+    }
+
+    public void selecionadoTesoura(View view){
+        this.opcaoSelecionado(2);
+    }
+
+    public void opcaoSelecionado(int opcaoUsuario){
+        ImageView imageResultado = findViewById(R.id.imageResultado);
+        TextView textView = findViewById(R.id.textResultado);
+
+        int opcaoApp = new Random().nextInt(3);
+    }
+
 }
